67944831b6ea5b4dc275f90f3d9cc56c77084994
Domchap90 <domchaple@gmail.com>
2020-06-16 15:52:36 +0000

    show_films and format_title function added and working.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, render_template, redirect, request, session, url_for
+from flask import Flask, flash, render_template, redirect, request, session, url_for, jsonify
 import requests
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -15,8 +15,6 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['API_KEY'] = os.environ.get('API_KEY')
 
 mongo = PyMongo(app)
-#url = 'http://img.omdbapi.com/?apikey=[yourkey]&'
-#datarequest = 'http://www.omdbapi.com/?apikey=[yourkey]&'
 
 @app.route('/')
 def home(): 
@@ -26,6 +24,7 @@ def home():
 @app.route('/insert_user', methods=['POST','GET'])
 def insert_user():
     login_collection = mongo.db.login
+    login_collection.delete_many({})
     usernames=login_collection.find({},{"username":1})
     session['username']=request.form.get('username')
     password=request.form.get('password')
@@ -55,35 +54,73 @@ def insert_rating(username):
     r=requests.get('https://www.omdbapi.com/?i='+f.get('imdbID')+'&apikey='+app.config['API_KEY'] )
     film_json=r.json()
     poster=film_json['Poster']
-    print('poster url is '+poster )
+    print('poster is '+poster)
     film_collection=mongo.db.films
     if film_collection.find_one({'imdbID':f.get('imdbID'), 'ratings':{'$elemMatch':{ 'username':session['username']}}}):
         """ updates rating from DB where film and this user's rating already exist """
         film_collection.update( 
             {'imdbID':f.get('imdbID'), 'ratings.username':session['username'] },
-            {'$set': {'ratings.$.rating':f.get('rating'), 'ratings.$.review':form['review']} }
+            {'$set': {'ratings.$.rating':int(f.get('rating')), 'ratings.$.review':form['review']} }
             )
         print("Rating updated for this film.")
     elif film_collection.find_one({'imdbID':f.get('imdbID')}):
         """ inserts rating into pre-existing film which this user hasn't rated yet """
         film_collection.update( 
             {'imdbID':f.get('imdbID') },
-            {'$push': { 'ratings' : {'username': session['username'] ,'rating':f.get('rating'), 'review':form['review']} }}
+            {'$push': { 'ratings' : {'username': session['username'] ,'rating':int(f.get('rating')), 'review':form['review']} }}
             )
         print("Rating added for this film. (option 2)")
     else:
         """ film not currently listed in db """
         film_collection.insert_one({
-            'title':form['film_title'],'imdbID':form['imdbID'],'year':f.get('Year'),'director':f.get('Director'), 
+            'title': format_title(form['film_title']),'imdbID':form['imdbID'],'year':f.get('Year'),'director':f.get('Director'), 
             'cast':f.get('Actors'), 'runtime':f.get('Runtime'), 'poster': poster,'ratings':[{'username':session['username'],
-            'rating':f.get('rating'), 'review':form['review'] }]
+            'rating':int(f.get('rating')), 'review':form['review'] }]
             }) 
         print("Rating + Film inserted. (option 3)")
 
     user_films = film_collection.find({'ratings':{'$elemMatch':{ 'username':session['username']}}})
     return redirect(url_for('userprofile',username=username, films=user_films))
 
+@app.route('/show_films', methods=['GET'])
+def show_films():
+    film_collection=mongo.db.films
+    film_rankings=film_collection.aggregate([
+        {'$addFields': {
+            'ratingsCount': {'$size':'$ratings'}
+            }
+        },
+        {"$unwind" : "$ratings"},
+        {"$group": {
+            'title' : { '$first': '$title' },
+            'year': { '$first': '$year' },
+            "_id":"$imdbID",
+            'ratingsCount': { '$first':"$ratingsCount"},
+            "averageRating" : 
+                {"$avg" : "$ratings.rating"}
+            }},
+        { '$sort' : { 'averageRating' : -1, 'ratingsCount': -1}},
+        ])
 
+    return render_template('films.html', film_rankings=film_rankings)
+
+def format_title(title):
+    """ Formats film titles so that the capitalization of each word is consistent with imdb formatting rules"""
+    title=title.lower()
+    # list of smaller words that are NOT capitalized in the middle of titles (but are capitalized if they are first/last word )
+    exceptions=["a", "an", "and", "as", "at", "by", "for", "from", "in", "of", "on", "or", "the", "to", "with"]
+    title_words=title.split()
+    formatted_title=[]
+    ind=0
+     
+    for word in title_words:
+        if word in exceptions and title_words.index(word,ind)>0 and title_words.index(word,ind)<len(title_words)-1: 
+            formatted_title.append(word)
+        else:
+            formatted_title.append(word.capitalize())
+        ind=ind+1
+
+    return " ".join(formatted_title)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP', '0.0.0.0'),
